0e028350160398638e027c3f3c9f73e953c97efc
Nigel Andrews <nigel.andrews@epita.fr>
2024-10-25 11:29:27 +0200

    env: nixify

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.default = with pkgs; mkShell {
+        packages = [
+          # programs
+          gcc14
+          gdb
+          gnumake
+          clang-tools
+          pre-commit
+
+          # libs
+          criterion
+        ];
+
+        hardeningDisable = [ "all" ];
+      };
+    }
+  );
+}
