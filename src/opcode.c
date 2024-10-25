4edb51deac221db97c701cdee32a88cfec9e6cff
Nigel Andrews <nigel.andrews@epita.fr>
2024-10-25 21:07:23 +0200

    feat: update condition flags after add

--- src/opcode.c
+++ src/opcode.c
@@ -3,6 +3,19 @@
 #include "bithelpers.h"
 #include "registers.h"
 
+static void update_condition_flags(int16_t registers[],
+                                   enum reg_t modified_register)
+{
+    int16_t result = registers[modified_register];
+
+    if (result < 0)
+        register_set(registers, RCOND, FLAG_N);
+    else if (result > 0)
+        register_set(registers, RCOND, FLAG_P);
+    else
+        register_set(registers, RCOND, FLAG_Z);
+}
+
 void op_add(int16_t registers[], uint16_t instruction_line)
 {
     int16_t rhs = -1;
@@ -13,11 +26,13 @@ void op_add(int16_t registers[], uint16_t instruction_line)
     }
     else
     {
-        rhs = registers[instruction_line & 0x7];
+        rhs = register_get(registers, instruction_line & 0x7);
     }
 
     int16_t result =
         register_get(registers, (instruction_line >> 6) & 0x7) + rhs;
 
-    register_set(registers, (instruction_line >> 9) & 0x7, result);
+    enum reg_t dr = (instruction_line >> 9) & 0x7;
+    register_set(registers, dr, result);
+    update_condition_flags(registers, dr);
 }
